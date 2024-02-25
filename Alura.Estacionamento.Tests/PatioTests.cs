b7e885641a103d040f2fdc8e2d2178425c2ebe8b
Felipe Toffoli <felipe_toffoli1@hotmail.com>
2024-02-25 14:14:47 -0400

    tests faturamento

---
+++ Alura.Estacionamento.Tests/PatioTests.cs
@@ -0,0 +1,39 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.Estacionamento.Tests
+{
+    public class PatioTests
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            // Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = "André Silva";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Vermelo";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "asd-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // Act
+            Double faturamento = estacionamento.TotalFaturado();
+
+            // Assert
+            Assert.Equal(2, faturamento);
+
+        }
+    }
+
+}
--- Alura.Estacionamento.Tests/VeiculoTest.cs
+++ Alura.Estacionamento.Tests/VeiculoTest.cs
@@ -9,16 +9,22 @@ namespace Alura.Estacionamento.Tests
         [Fact]
         public void TestaAcelerar()
         {
+            //Arrange
             var veiculo = new Veiculo();
+            //Act
             veiculo.Acelerar(10);
+            //Assert
             Assert.Equal(100, veiculo.VelocidadeAtual);
         }
 
         [Fact]
         public void TestaFreiar()
         {
+            //Arrange
             var veiculo = new Veiculo();
+            //Act
             veiculo.Frear(10);
+            //Assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
    
